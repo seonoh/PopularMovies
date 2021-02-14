5a8af0d05bf12eb014a4321f1d9363d1a981565e
Seonoh Eam <eso0609@naver.com>
2021-02-14 23:38:03 +0900

    add api test model

---
+++ app/src/main/java/com/example/seonoh/popularmovies/data/vo/MovieDetails.kt
@@ -0,0 +1,9 @@
+package com.example.seonoh.popularmovies.data.vo
+
+
+// api test model
+data class MovieDetails (
+        val budget : Int,
+        val homepage : String,
+        val id : Int
+)
