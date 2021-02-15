79eff856d6e800a07c5f927c0234350a79c02eea
Seonoh Eam <eso0609@naver.com>
2021-02-15 16:29:24 +0900

    add movie details repository

---
+++ app/src/main/java/com/example/seonoh/popularmovies/single_movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,24 @@
+package com.example.seonoh.popularmovies.single_movie_details
+
+import androidx.lifecycle.LiveData
+import com.example.seonoh.popularmovies.data.api.TheMovieDBInterface
+import com.example.seonoh.popularmovies.data.repository.MovieDetailsNetworkDataSource
+import com.example.seonoh.popularmovies.data.repository.NetworkState
+import com.example.seonoh.popularmovies.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository(private val apiService : TheMovieDBInterface) {
+
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails(compositeDisposable: CompositeDisposable,movieId : Int) : LiveData<MovieDetails>{
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService,compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieResponse
+    }
+
+    fun getMovieDetailsNetworkState():LiveData<NetworkState>{
+        return movieDetailsNetworkDataSource.networkState
+    }
+}
