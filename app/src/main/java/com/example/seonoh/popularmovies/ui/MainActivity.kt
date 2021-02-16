522856e190f69b30adcb8a876b6446a8ca925e86
Seonoh Eam <eso0609@naver.com>
2021-02-16 17:54:02 +0900

    apply databinding

--- app/src/main/java/com/example/seonoh/popularmovies/ui/MainActivity.kt
+++ app/src/main/java/com/example/seonoh/popularmovies/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.seonoh.popularmovies.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.seonoh.popularmovies.R
+import com.example.seonoh.popularmovies.databinding.ActivityMainBinding
+import com.example.seonoh.popularmovies.ui.single_movie_details.SingleMovie
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.run {
+            btn.setOnClickListener {
+                val intent = Intent(this@MainActivity, SingleMovie::class.java)
+                intent.putExtra("id",299534)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/seonoh/popularmovies/ui/single_movie_details/SingleMovie.kt
+++ app/src/main/java/com/example/seonoh/popularmovies/ui/single_movie_details/SingleMovie.kt
@@ -2,25 +2,31 @@ package com.example.seonoh.popularmovies.ui.single_movie_details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.seonoh.popularmovies.R
+import com.example.seonoh.popularmovies.data.api.POSTER_BASE_URL
 import com.example.seonoh.popularmovies.data.api.TheMovieDBClient
 import com.example.seonoh.popularmovies.data.api.TheMovieDBInterface
 import com.example.seonoh.popularmovies.data.vo.MovieDetails
+import com.example.seonoh.popularmovies.databinding.ActivitySingleMovieBinding
+import java.text.NumberFormat
+import java.util.*
 
 class SingleMovie : AppCompatActivity() {
 
+    private lateinit var binding : ActivitySingleMovieBinding
     private lateinit var viewModel : SingleMovieViewModel
     private lateinit var movieRepository : MovieDetailsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_movie)
+        binding =DataBindingUtil.setContentView(this,R.layout.activity_single_movie)
 
-        val movieId : Int =1
+        val movieId : Int = intent.getIntExtra("id",1)
 
         val apiService : TheMovieDBInterface = TheMovieDBClient.getClient()
         movieRepository = MovieDetailsRepository(apiService)
@@ -33,6 +39,20 @@ class SingleMovie : AppCompatActivity() {
     }
 
     fun bindUI(it : MovieDetails){
+        binding.run {
+            movieTitle.text = it.title
+            movieTagline.text = it.tagline
+            movieReleaseDate.text = it.releaseDate
+            movieRating.text = it.rating.toString()
+            movieRuntime.text = it.runtime.toString()+" minute"
+
+            val formatCurrency = NumberFormat.getCurrencyInstance(Locale.US)
+            movieBudget.text = formatCurrency.format((it.budget))
+            movieRevenue.text = formatCurrency.format((it.revenue))
+
+            val moviePosterURL = POSTER_BASE_URL + it.posterPath
+
+        }
     }
 
     private fun getViewModel(movieId : Int) : SingleMovieViewModel{
