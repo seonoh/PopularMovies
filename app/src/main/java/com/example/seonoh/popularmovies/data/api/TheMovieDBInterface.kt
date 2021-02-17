4b65803f559b5aa3cd69cd498c92f4a7bccd7b3a
Seonoh Eam <eso0609@naver.com>
2021-02-17 19:12:30 +0900

    add movie popular api && response model

--- app/src/main/java/com/example/seonoh/popularmovies/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/example/seonoh/popularmovies/data/api/TheMovieDBInterface.kt
@@ -1,14 +1,18 @@
 package com.example.seonoh.popularmovies.data.api
 
 import com.example.seonoh.popularmovies.data.vo.MovieDetails
+import com.example.seonoh.popularmovies.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
-
+import retrofit2.http.Query
 
 
 interface TheMovieDBInterface  {
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id:Int) : Single<MovieDetails>
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page:Int) : Single<MovieResponse>
 }
---
+++ app/src/main/java/com/example/seonoh/popularmovies/data/vo/MovieResponse.kt
@@ -0,0 +1,13 @@
+package com.example.seonoh.popularmovies.data.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+        val page: Int,
+        val movies: List<Movie>,
+        @SerializedName("total_pages")
+        val totalPages: Int,
+        @SerializedName("total_results")
+        val totalResults: Int
+)
