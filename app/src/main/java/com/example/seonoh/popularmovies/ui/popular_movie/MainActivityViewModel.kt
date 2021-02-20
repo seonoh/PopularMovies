8331e9d98f03adcefcc7e431753fceda11566b9d
Seonoh Eam <eso0609@naver.com>
2021-02-20 22:43:25 +0900

    add MainActivityViewModel

---
+++ app/src/main/java/com/example/seonoh/popularmovies/ui/popular_movie/MainActivityViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.seonoh.popularmovies.ui.popular_movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import com.example.seonoh.popularmovies.data.repository.NetworkState
+import com.example.seonoh.popularmovies.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MainActivityViewModel(private val movieRepository: MoviePagedListRepository) : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    private val moviePagedList: LiveData<PagedList<Movie>> by lazy {
+        movieRepository.fetchLiveMoviePagedList(compositeDisposable)
+    }
+
+    val networkState: LiveData<NetworkState> by lazy {
+        movieRepository.getNetworkState()
+    }
+
+    fun listIsEmpty(): Boolean = moviePagedList.value?.isEmpty() ?: true
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
