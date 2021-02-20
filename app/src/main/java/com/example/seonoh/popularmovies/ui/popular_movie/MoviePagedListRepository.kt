2fb1a1f7ff9ef630c235ea723907c25c755f3a25
Seonoh Eam <eso0609@naver.com>
2021-02-20 22:43:08 +0900

    add MoviePagedListRepository

--- app/src/main/java/com/example/seonoh/popularmovies/ui/popular_movie/MoviePagedListRepository.kt
+++ app/src/main/java/com/example/seonoh/popularmovies/ui/popular_movie/MoviePagedListRepository.kt
@@ -1,4 +1,37 @@
 package com.example.seonoh.popularmovies.ui.popular_movie
 
-class MoviePagedListRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.seonoh.popularmovies.data.api.POST_PER_PAGE
+import com.example.seonoh.popularmovies.data.api.TheMovieDBInterface
+import com.example.seonoh.popularmovies.data.repository.MovieDataSource
+import com.example.seonoh.popularmovies.data.repository.MovieDataSourceFactory
+import com.example.seonoh.popularmovies.data.repository.NetworkState
+import com.example.seonoh.popularmovies.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MoviePagedListRepository(private val apiService : TheMovieDBInterface) {
+
+    lateinit var moviePagedList : LiveData<PagedList<Movie>>
+    lateinit var moviesDataSourceFactory: MovieDataSourceFactory
+
+    fun fetchLiveMoviePagedList (compositeDisposable: CompositeDisposable) : LiveData<PagedList<Movie>>{
+        moviesDataSourceFactory = MovieDataSourceFactory(apiService,compositeDisposable)
+
+        val config = PagedList.Config.Builder()
+                .setEnablePlaceholders(false)
+                .setPageSize(POST_PER_PAGE)
+                .build()
+
+        moviePagedList = LivePagedListBuilder(moviesDataSourceFactory,config).build()
+        return moviePagedList
+    }
+
+    fun getNetworkState() : LiveData<NetworkState>{
+        return Transformations.switchMap<MovieDataSource, NetworkState>(
+                moviesDataSourceFactory.moviesLiveDataSource,MovieDataSource::networkState
+        )
+    }
 }
