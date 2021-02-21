af8bf10568b123a2e4a68d8c66d69deb265db839
Seonoh Eam <eso0609@naver.com>
2021-02-21 13:07:20 +0900

    apply viewholder databinding

--- app/src/main/java/com/example/seonoh/popularmovies/ui/popular_movie/PopularMoviePagedListAdapter.kt
+++ app/src/main/java/com/example/seonoh/popularmovies/ui/popular_movie/PopularMoviePagedListAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.seonoh.popularmovies.ui.popular_movie
 
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.seonoh.popularmovies.data.api.POSTER_BASE_URL
 import com.example.seonoh.popularmovies.data.repository.NetworkState
 import com.example.seonoh.popularmovies.data.vo.Movie
+import com.example.seonoh.popularmovies.databinding.MovieListItemBinding
+import com.example.seonoh.popularmovies.ui.single_movie_details.SingleMovie
 
 class PopularMoviePagedListAdapter : PagedListAdapter<Movie, RecyclerView.ViewHolder>(MovieDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -22,10 +27,26 @@ class PopularMoviePagedListAdapter : PagedListAdapter<Movie, RecyclerView.ViewHo
         override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean = oldItem == newItem
     }
 
-    class MovieItemViewHolder ( view : View) : RecyclerView.ViewHolder(view){
+    class MovieItemViewHolder ( private val binding : MovieListItemBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun bind(movie : Movie?, context : Context){
-//            itemView.rootView.cv
+            binding.run {
+                cvMovieTitle.text = movie?.title
+                cvMovieReleaseDate.text = movie?.releaseDate
+
+                val moviePosterURL = POSTER_BASE_URL + movie?.posterPath
+                Glide.with(binding.root.context)
+                        .load(moviePosterURL)
+                        .into(cvIvMoviePoster)
+
+                root.setOnClickListener {
+                    val intent = Intent(context, SingleMovie::class.java)
+                    intent.putExtra("id",movie?.id)
+                    context.startActivity(intent)
+                }
+
+                executePendingBindings()
+            }
         }
 
     }
